70b5786fbfc981946770d37cd6d198db572d3494
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2022-08-09 18:55:25 -0300

    chore: separating dependencies to another file

---
+++ src/test/java/com/mkdata/company/CompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkdata.company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
