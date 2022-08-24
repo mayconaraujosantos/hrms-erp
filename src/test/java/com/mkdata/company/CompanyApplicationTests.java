4bbab7a07553cc04f2ec4539adc038be744ca702
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2022-08-23 23:21:25 -0300

    test: add simple test to companyApplication

--- src/test/java/com/mkdata/company/CompanyApplicationTests.java
+++ src/test/java/com/mkdata/company/CompanyApplicationTests.java
@@ -1,13 +1,17 @@
 package com.mkdata.company;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompanyApplicationTests {
-
+	private static boolean isAdmin = true;
+	
 	@Test
 	void contextLoads() {
+		assertTrue(isAdmin);
 	}
 
 }
