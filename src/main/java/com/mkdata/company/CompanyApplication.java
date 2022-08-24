cdc6c500838d95e66ea6047aed79aca10b2730b6
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2022-08-24 00:42:35 -0300

    chore: add exclude data source configuration for CI CD Github Actions

--- src/main/java/com/mkdata/company/CompanyApplication.java
+++ src/main/java/com/mkdata/company/CompanyApplication.java
@@ -2,8 +2,9 @@ package com.mkdata.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class})
 public class CompanyApplication {
 
 	public static void main(String[] args) {
