7a5e553ebc101e19be270bb1e2d229b3ce4d8625
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2022-08-26 00:00:52 -0300

    chore: add checkstyle plugin configuration

--- src/test/java/com/mkdata/company/CompanyApplicationTests.java
+++ src/test/java/com/mkdata/company/CompanyApplicationTests.java
@@ -1,15 +1,21 @@
 package com.mkdata.company;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompanyApplicationTests {
-	private static boolean isAdmin = true;
+	private static boolean IS_ADMIN = true;
 	
+	/**
+	 * 
+	 */
 	@Test
 	void contextLoads() {
 		CompanyApplication.main(new String[] {});
+		assertTrue(IS_ADMIN);
 	}
 
 }
