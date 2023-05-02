a705d1fc839409fc4eb5aff23d1b1121c3fde686
mayconaraujosantos <mayconar.aujo.santos@gmail.com>
2023-05-02 01:44:02 -0300

    chore: add simple lambda test

--- src/test/java/com/mkdata/company/CompanyApplicationTests.java
+++ src/test/java/com/mkdata/company/CompanyApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mkdata.company;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,15 +8,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompanyApplicationTests {
-	private static boolean IS_ADMIN = true;
-	
-	/**
-	 * 
-	 */
-	@Test
-	void contextLoads() {
-		CompanyApplication.main(new String[] {});
-		assertTrue(IS_ADMIN);
-	}
 
+  private static boolean IS_ADMIN = true;
+
+  /**
+   *
+   */
+  @Test
+  void contextLoads() {
+    String[] args = { "" };
+    Runnable runnable = () -> CompanyApplication.main(args);
+    assertNotNull(runnable);
+  }
 }
