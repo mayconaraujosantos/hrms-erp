09bd5a5e1bfd1689d5277276be9f18e0c8ce04ed
mayconaraujosantos <mayconar.aujo.santos@gmail.com>
2023-05-04 21:41:19 -0300

    test: create test for main

--- src/main/java/com/mkdata/company/CompanyApplication.java
+++ src/main/java/com/mkdata/company/CompanyApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CompanyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompanyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyApplication.class, args);
+    }
 }
--- src/test/java/com/mkdata/company/CompanyApplicationTests.java
+++ src/test/java/com/mkdata/company/CompanyApplicationTests.java
@@ -1,6 +1,6 @@
 package com.mkdata.company;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,10 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CompanyApplicationTests {
 
-  @Test
-  void contextLoads() {
-    String[] args = { "" };
-    Runnable runnable = () -> CompanyApplication.main(args);
-    Assertions.assertNotNull(runnable);
-  }
+    @Test
+    void contextLoads() {
+        String[] args = {""};
+        Runnable runnable = () -> CompanyApplication.main(args);
+        Assertions.assertNotNull(runnable);
+    }
+
+    @Test
+    void testMain() {
+        CompanyApplication.main(new String[] {});
+        assertTrue(true);
+    }
 }
