5115ebd0894c006da863dbbc30d68397f3f49deb
mayconaraujosantos <mayconar.aujo.santos@gmail.com>
2023-05-08 16:23:02 -0300

    chore: enum created with best description

---
+++ src/main/java/com/mkdata/company/domain/enums/SalaryType.java
@@ -0,0 +1,23 @@
+package com.mkdata.company.domain.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SalaryType {
+    HOURLY("Hourly", "Per hour"),
+    MONTHLY("Monthly", "Per month"),
+    YEARLY("Yearly", "Per year");
+
+    private final String type;
+    private final String description;
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
