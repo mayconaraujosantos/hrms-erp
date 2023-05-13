260c51cd29bf2af0004e5f501a6d113d773971bd
mayconaraujosantos <mayconar.aujo.santos@gmail.com>
2023-05-13 19:37:40 -0300

    chore: implement use case for job posting

--- src/main/java/com/mkdata/company/CompanyApplication.java
+++ src/main/java/com/mkdata/company/CompanyApplication.java
@@ -2,12 +2,15 @@ package com.mkdata.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories
 public class CompanyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CompanyApplication.class, args);
+        SpringApplication application = new SpringApplication(CompanyApplication.class);
+        application.setAdditionalProfiles("prod");
+        application.run(args);
     }
 }
---
+++ src/main/java/com/mkdata/company/app/dto/JobPostingDTORequest.java
@@ -0,0 +1,35 @@
+package com.mkdata.company.app.dto;
+
+import com.mkdata.company.domain.enums.EducationLevel;
+import com.mkdata.company.domain.enums.ExperienceLevel;
+import com.mkdata.company.domain.enums.JobStatus;
+import com.mkdata.company.domain.enums.JobType;
+import com.mkdata.company.domain.enums.SalaryType;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class JobPostingDTORequest {
+    private String title;
+    private String description;
+    private String companyName;
+    private String location;
+    private String jobDescription;
+    private Double salary;
+    private JobType jobType;
+    private SalaryType salaryType;
+    private ExperienceLevel experienceLevel;
+    private EducationLevel educationLevel;
+    private JobStatus jobStatus;
+    private String requirements;
+    private String responsibilities;
+    private String benefits;
+    private Date jobPostedDate;
+    private Date applicationDeadline;
+}
